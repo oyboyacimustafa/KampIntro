7a70efe82ac9f2e7ece6e0908f6826438420c212
rampage <rampage@DESKTOP-96BV97D>
2021-06-03 00:25:13 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not yourself - kendini tekrarlama
+            // alias - değer tutucu
+
+            string kategoriEtiketi = "kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("Giris Yap Butonu");
+            }
+
+
+
+        }
+    }
+}
